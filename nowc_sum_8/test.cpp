4839bb239aeac3e3bfe41de537f4c9bad1b68e76
LAOBIEXL <2184769952@qq.com>
2026-08-12 23:32:00 +0800

    add:新增牛客暑期训练8练习代码

---
+++ nowc_sum_8/test.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MOD = 998244353;
+void solve()
+{
+    int n, m;
+    cin >>n >> m;
+    set<int> st;
+    int flag = 0;
+    for(int i = 0; i < m;i++)
+    {
+        int t;
+        cin >> t;
+        st.insert(t);
+        if(t >= 2 * n)
+        {
+            cout << 0 << endl;
+            flag = true;
+            //return;
+        }
+    }
+    if(flag)
+    {
+        return;
+    }
+    vector<int> dp(n + 2) ;
+    dp[0] = 1;
+    for(int i = 1;i <= 2*n; i++)
+    {
+        if(st.count(i))
+        {
+            for(int j = n; j >= 1; j--)
+            {
+                dp[j] = dp[j - 1];
+            }
+        }
+        else
+        {
+            vector<int> pre(dp);
+            int pre1 = dp[1];
+            for(int j = n;j >= 1;j--)
+            {
+                dp[j] = (dp[j - 1] + pre[j + 1]) % MOD;
+            }
+            dp[0] = pre[1];
+        }
+    }
+    cout << dp[0] % MOD << endl;
+    // for(int i = 0; i <= 2 * n ;i++)
+    // {
+    //     for(int j = 0;j <= n + 1;j ++)
+    //     {
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+
+    return 0;
+}
